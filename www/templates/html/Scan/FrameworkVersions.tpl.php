9d061611aa9d3d3580787d53ba358e8aa7345589
Michael Fairchild <mfairchild365@gmail.com>
2014-06-12 16:41:29 -0500

    Sort by framework versions desc

--- www/templates/html/Scan/FrameworkVersions.tpl.php
+++ www/templates/html/Scan/FrameworkVersions.tpl.php
@@ -10,7 +10,7 @@ $site           = $scan->getSite();
             This is a list of all framework versions that we found on your site.
         </div>
     </header>
-    <table data-sortlist="[[0,0],[2,0]]" class="sortable">
+    <table data-sortlist="[[1,1],[2,1]]" class="sortable">
         <thead>
         <tr>
             <th class="path">Path</th>
