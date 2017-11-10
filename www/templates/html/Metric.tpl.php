2e612b10e1724542c52445b5fcb7fc6876cb7362
Michael Fairchild <mfairchild365@gmail.com>
2017-11-10 15:01:00 -0600

    Allow for custom metric messages

--- www/templates/html/Metric.tpl.php
+++ www/templates/html/Metric.tpl.php
@@ -1,3 +1,10 @@
 <p>
     The UNLedu Framework Checker audits your pages to ensure that the most recent framework versions are used. It will also check for various University of Nebraska–Lincoln specific standards.
 </p>
+<?php
+$included_file = __DIR__.'/../../../custom-message.html';
+if (file_exists($included_file)) {
+    include $included_file;
+}
+
+?>
